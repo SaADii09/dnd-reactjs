69e5533aea5f5e3b8adca32faa3d52766958eff6
Saad Ahmad <saadahmad6830879@gmail.com>
2025-04-29 06:24:05 +0500

    ds

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import { HTML5Backend } from "react-dnd-html5-backend";
 import RealDnd from "./Pages/RealDnd";
 
 function App() {
-	return (
-		<DndProvider backend={HTML5Backend}>
-			<RealDnd />
-		</DndProvider>
-	);
+    return (
+        <DndProvider backend={HTML5Backend}>
+            <RealDnd />
+        </DndProvider>
+    );
 }
 
 export default App;
