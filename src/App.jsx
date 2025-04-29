031aa8e255de053fb7b1cebc5fc916bc8b319210
Saad Ahmad <saadahmad6830879@gmail.com>
2025-04-29 06:30:29 +0500

    fix: correct import path for RealDnd component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
-import RealDnd from "./Pages/RealDnd";
+import RealDnd from "./pages/RealDnd";
 
 function App() {
     return (
